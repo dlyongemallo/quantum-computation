7fedd30e86a21948bb4dbc08d0c3cf2b7062a88e
D.L.Yonge.Mallo <D.L.Yonge.Mallo@gmail.com>
2013-12-25 14:16:14 +0000

    add dj_probs.m

---
+++ octave/dj_probs.m
@@ -0,0 +1,18 @@
+n = 3;
+N = 2**n;
+
+% Hamming weights.
+kvec = 0:1:N/2-1;
+
+% Counts of hidden strings with that Hamming weight.
+cvec = arrayfun(@(k) nchoosek(N,k), kvec)
+
+% Probability of a specific hidden string of the Hamming weight.
+pvec = arrayfun(@(k) (1 - 2*k/N)**2, kvec)
+
+% Probability of any hidden string of the Hamming weight.
+total = 2**N / N
+mvec = cvec .* pvec / (total / 2)
+
+% Cumulative sum of probabilities.
+svec = cumsum(mvec)
