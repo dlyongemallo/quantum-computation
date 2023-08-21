7eaaf059fe6a8d2df1932778744d76dd0e0752f8
David Yonge-Mallo <dlyongemallo@users.noreply.github.com>
2023-08-21 13:10:19 +0200

    Update qiskit optimization example to qiskit 0.44.

--- qiskit/qiskit_optimization_module.py
+++ qiskit/qiskit_optimization_module.py
@@ -1,8 +1,9 @@
 # Copied from: https://gist.github.com/ismaelfaro/2f9351259bb8107db0d06d774109b2b8
 # example to solve a quadratic problem using a Quantum optimization algorithm
 
-from qiskit.optimization import QuadraticProgram
-from qiskit.optimization.algorithms import GroverOptimizer
+from qiskit_optimization import QuadraticProgram
+from qiskit_optimization.algorithms import GroverOptimizer
+from qiskit_optimization.translators import from_docplex_mp
 
 from qiskit import Aer
 
@@ -14,8 +15,7 @@ x, y = model.binary_var_list(2)
 model.maximize(x*y + x + y)
 
 # convert DOcplex model to Qiskit Quadratic Program
-qp = QuadraticProgram()
-qp.from_docplex(model)
+qp = from_docplex_mp(model)
 
 # define a Quantum backend on which to run the optimization
 # a Qiskit simulator in this case
