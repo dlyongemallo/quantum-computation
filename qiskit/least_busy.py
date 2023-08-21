d9a4f1e444e62dca74778156392d28897088da05
David Yonge-Mallo <dlyongemallo@users.noreply.github.com>
2023-08-21 12:32:11 +0200

    Update least_busy to use new provider.

--- qiskit/least_busy.py
+++ qiskit/least_busy.py
@@ -3,12 +3,10 @@
 """Find the least busy device backend on IBMQ.
 """
 
-from qiskit import IBMQ
-from qiskit.providers.ibmq import least_busy
+from qiskit_ibm_provider import IBMProvider, least_busy
 
 # Register with the API.
-IBMQ.load_account()
-provider = IBMQ.get_provider(hub='ibm-q')
+provider = IBMProvider(instance='ibm-q/open/main')
 
 # Get the least busy backend satisfying requirements.
 backend = least_busy(provider.backends(
